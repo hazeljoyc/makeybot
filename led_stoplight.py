787bc3547a8530ece1d11dab594d36dc5cbc903c
hazeljoyc <hazeljgchavez@gmail.com>
2025-03-13 08:42:59 -0700

    Update led_stoplight.py

--- led_stoplight.py
+++ led_stoplight.py
@@ -1,3 +1,20 @@
+# ============================================================================
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/hazeljoyc/makeybot
+# Hacker: Hazel Joy Chavez
+# This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# https://eupl.eu/
+# Program/Design Name:        led_stoplight.py
+# Description:    This is a program to turn on and off a set of 3 LEDs.
+# program description:
+# 1) Read values from traffic_light library
+# 2) Check status of each light
+# 3) Turn on light depending on value
+# Dependencies:   python3
+# ============================================================================
+
 from gpiozero import LED
 import time
 
